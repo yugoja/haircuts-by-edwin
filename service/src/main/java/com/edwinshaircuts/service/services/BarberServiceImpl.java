ef9c14828ddb1f961f57c00155af578b70cc23ef
Yuvraj Jadhav <yjadha14@in.ibm.com>
2020-07-16 21:48:20 +0530

    Remove unused imports from BarberServiceImpl

--- service/src/main/java/com/edwinshaircuts/service/services/BarberServiceImpl.java
+++ service/src/main/java/com/edwinshaircuts/service/services/BarberServiceImpl.java
@@ -2,10 +2,7 @@ package com.edwinshaircuts.service.services;
 
 import com.edwinshaircuts.service.vo.Barber;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.annotation.Order;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,7 +11,6 @@ import java.util.List;
 public class BarberServiceImpl implements BarberService{
     @Autowired
     MongoTemplate mongoTemplate;
-
 
     @Override
     public List<Barber> getBarbers() {
